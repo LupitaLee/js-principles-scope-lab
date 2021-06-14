6fad859d8e685d9dcddb6c003e82ab9248213520
Lupita <lupitarivera8899@gmail.com>
2021-06-14 16:00:06 -0500

    commit tree 3230274737e774ad95dd610b9c1ac65a00f04a54

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+
+var customerName = 'bob'
+const leastFavoriteCustomer = 'maybe bob'
+
+
+function upperCaseCustomerName(){
+    return customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer(){
+   bestCustomer = 'not bob'
+    return bestCustomer 
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+    return bestCustomer 
+}
+function changeLeastFavoriteCustomer(){
+    return leastFavoriteCustomer = 'sorry bob'
+}
